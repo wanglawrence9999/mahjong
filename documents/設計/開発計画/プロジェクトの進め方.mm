17e55286dadde70e3e3838297d32f63d788f0169
urushibaray <urushibaray@867d8d17-13de-45a2-9b0c-8d4e0d86bef1>
2009-07-01 14:18:01 +0000

    <refs #17594> 開発計画のマインドマップを追加しました。

---
+++ documents/設計/開発計画/プロジェクトの進め方.mm
@@ -0,0 +1,127 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1246405640835" ID="Freemind_Link_1302255794" MODIFIED="1246405708971" TEXT="&#x30d7;&#x30ed;&#x30b8;&#x30a7;&#x30af;&#x30c8;&#x306e;&#x9032;&#x3081;&#x65b9;">
+<node CREATED="1246405713962" ID="_" MODIFIED="1246405774977" POSITION="right" TEXT="&#x540d;&#x524d;">
+<node CREATED="1246405724919" ID="Freemind_Link_1881162416" MODIFIED="1246405728227" TEXT="Andjong">
+<node CREATED="1246405741368" ID="Freemind_Link_618673296" MODIFIED="1246405756745" TEXT="Android + Mahjong"/>
+</node>
+</node>
+<node CREATED="1246405763119" ID="Freemind_Link_1939544297" MODIFIED="1246405772092" POSITION="right" TEXT="&#x76ee;&#x7684;">
+<node CREATED="1246405824687" ID="Freemind_Link_1606157653" MODIFIED="1246406249192" TEXT="&#x30aa;&#x30fc;&#x30d7;&#x30f3;&#x30bd;&#x30fc;&#x30b9;&#x306e;&#x9ebb;&#x96c0;&#x30a2;&#x30d7;&#x30ea;&#x306e;&#x958b;&#x767a;">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1246405915301" ID="Freemind_Link_1550855510" MODIFIED="1246405921630" TEXT="&#x5b66;&#x7fd2;">
+<node CREATED="1246405922869" ID="Freemind_Link_1079597976" MODIFIED="1246406255490" TEXT="Java">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1246406037109" ID="Freemind_Link_765836495" MODIFIED="1246406258609" TEXT="&#x30aa;&#x30d6;&#x30b8;&#x30a7;&#x30af;&#x30c8;&#x6307;&#x5411;">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1246405925074" ID="Freemind_Link_1250085616" MODIFIED="1246406260091" TEXT="Google Android">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+</node>
+<node CREATED="1246407411436" ID="Freemind_Link_908561365" MODIFIED="1246407679534" POSITION="right" TEXT="&#x5236;&#x9650;">
+<icon BUILTIN="clanbomber"/>
+<node CREATED="1246407439724" ID="Freemind_Link_839494352" MODIFIED="1246407450471" TEXT="&#x6642;&#x9593;&#x304c;&#x306a;&#x3044;">
+<node CREATED="1246407456422" ID="Freemind_Link_1129442765" MODIFIED="1246407463654" TEXT="200907&#x306f;&#x5fd9;&#x3057;&#x3044;"/>
+</node>
+<node CREATED="1246406076938" ID="Freemind_Link_1000037344" MODIFIED="1246407663049" TEXT="&#x958b;&#x767a;&#x671f;&#x9593;&#x304c;&#x9650;&#x3089;&#x308c;&#x3066;&#x3044;&#x308b;">
+<node CREATED="1246406091635" ID="Freemind_Link_1819429146" MODIFIED="1246406114066" TEXT="Google Android&#x643a;&#x5e2f;&#x306e;&#x767a;&#x58f2;&#x9593;&#x8fd1;"/>
+</node>
+</node>
+<node CREATED="1246405804069" ID="Freemind_Link_283584102" MODIFIED="1246405807483" POSITION="right" TEXT="&#x624b;&#x6cd5;">
+<node CREATED="1246406190081" ID="Freemind_Link_1343110428" MODIFIED="1246406753877" TEXT="&#x30a6;&#x30a9;&#x30fc;&#x30bf;&#x30fc;&#x30d5;&#x30a9;&#x30fc;&#x30eb;">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1246406324812" ID="Freemind_Link_871308371" MODIFIED="1246406332327" TEXT="&#x77ed;&#x671f;&#x9593;&#x3067;&#x306e;&#x958b;&#x767a;&#x306f;&#x96e3;&#x3057;&#x3044;"/>
+<node CREATED="1246406337041" ID="Freemind_Link_1555762160" MODIFIED="1246406351322" TEXT="&#x4ed5;&#x69d8;&#x66f8;&#x306b;&#x3053;&#x3060;&#x308f;&#x308b;&#x5fc5;&#x8981;&#x306f;&#x306a;&#x3044;"/>
+</node>
+<node CREATED="1246406922010" ID="Freemind_Link_957531672" MODIFIED="1246407385647" TEXT="&#x30a2;&#x30b8;&#x30e3;&#x30a4;&#x30eb;&#x30bd;&#x30d5;&#x30c8;&#x30a6;&#x30a7;&#x30a2;&#x958b;&#x767a;">
+<icon BUILTIN="bookmark"/>
+<node CREATED="1246406943321" ID="Freemind_Link_1775059633" MODIFIED="1246406945880" TEXT="XP">
+<node CREATED="1246406946473" ID="Freemind_Link_504735717" MODIFIED="1246406956863" TEXT="&#x30a8;&#x30af;&#x30b9;&#x30c8;&#x30ea;&#x30fc;&#x30e0;&#x30fb;&#x30d7;&#x30ed;&#x30b0;&#x30e9;&#x30df;&#x30f3;&#x30b0;"/>
+<node CREATED="1246407156029" ID="Freemind_Link_520462165" MODIFIED="1246407159574" TEXT="&#x30d7;&#x30e9;&#x30af;&#x30c6;&#x30a3;&#x30b9;">
+<node CREATED="1246407160527" ID="Freemind_Link_352379563" MODIFIED="1246407166944" TEXT="&#x8272;&#x3005;&#x3042;&#x308b;">
+<node CREATED="1246407170146" ID="Freemind_Link_1571883626" MODIFIED="1246407192928" TEXT="Wikipedia&#x3092;&#x53c2;&#x7167;"/>
+</node>
+<node CREATED="1246407199783" ID="Freemind_Link_841989969" MODIFIED="1246407210761" TEXT="&#x3059;&#x3079;&#x3066;&#x306e;&#x30d7;&#x30e9;&#x30af;&#x30c6;&#x30a3;&#x30b9;&#x306e;&#x9054;&#x6210;&#x306f;&#x96e3;&#x3057;&#x3044;"/>
+<node CREATED="1246407280904" ID="Freemind_Link_251674406" MODIFIED="1246407282716" TEXT="&#x63a1;&#x7528;">
+<node CREATED="1246407285058" ID="Freemind_Link_385258047" MODIFIED="1246407287556" TEXT="&#x53cd;&#x5fa9;"/>
+<node CREATED="1246407295458" ID="Freemind_Link_1119341776" MODIFIED="1246407296676" TEXT="&#x30c6;&#x30b9;&#x30c8;&#x99c6;&#x52d5;&#x958b;&#x767a;"/>
+<node CREATED="1246407302703" ID="Freemind_Link_340799997" MODIFIED="1246407303796" TEXT="&#x30ea;&#x30d5;&#x30a1;&#x30af;&#x30bf;&#x30ea;&#x30f3;&#x30b0;"/>
+<node CREATED="1246407328765" ID="Freemind_Link_1082684948" MODIFIED="1246407329936" TEXT="YAGNI"/>
+<node CREATED="1246407359405" ID="Freemind_Link_1184837925" MODIFIED="1246407360280" TEXT="&#x77ed;&#x671f;&#x30ea;&#x30ea;&#x30fc;&#x30b9;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1246405807800" ID="Freemind_Link_1313834624" MODIFIED="1246405811153" POSITION="right" TEXT="&#x30de;&#x30a4;&#x30eb;&#x30b9;&#x30c8;&#x30fc;&#x30f3;">
+<node CREATED="1246407750375" ID="Freemind_Link_1899166454" MODIFIED="1246407764831" TEXT="&#xff5e;20090705">
+<node CREATED="1246407965540" ID="Freemind_Link_801002915" MODIFIED="1246407967508" TEXT="&#x30bf;&#x30b9;&#x30af;">
+<node CREATED="1246408259093" ID="Freemind_Link_321665112" MODIFIED="1246408263623" TEXT="&#x30c6;&#x30b9;&#x30c8;&#x4f5c;&#x6210;"/>
+<node CREATED="1246407818513" ID="Freemind_Link_880328429" MODIFIED="1246407830727" TEXT="Core&#x30b3;&#x30f3;&#x30dd;&#x30fc;&#x30cd;&#x30f3;&#x30c8;&#x306e;&#x958b;&#x767a;"/>
+<node CREATED="1246407892917" ID="Freemind_Link_264167592" MODIFIED="1246408109822" TEXT="AI&#x30af;&#x30e9;&#x30b9;&#x30d9;&#x30fc;&#x30b9;&#x306e;&#x958b;&#x767a;"/>
+<node CREATED="1246407925684" ID="Freemind_Link_1948600284" MODIFIED="1246413051762" TEXT="UI&#x30af;&#x30e9;&#x30b9;&#xff08;&#x30b3;&#x30f3;&#x30bd;&#x30fc;&#x30eb;&#xff09;&#x306e;&#x958b;&#x767a;"/>
+</node>
+<node CREATED="1246407946159" ID="Freemind_Link_1694370898" MODIFIED="1246407949313" TEXT="&#x30ea;&#x30ea;&#x30fc;&#x30b9;">
+<node CREATED="1246407953437" ID="Freemind_Link_636917631" MODIFIED="1246408599815" TEXT="&#x30b3;&#x30f3;&#x30bd;&#x30fc;&#x30eb;&#x3067;&#x52d5;&#x304f;&#x9ebb;&#x96c0;&#x30a2;&#x30d7;&#x30ea;">
+<icon BUILTIN="licq"/>
+</node>
+</node>
+</node>
+<node CREATED="1246407866476" ID="Freemind_Link_1822965560" MODIFIED="1246407874394" TEXT="&#xff5e;20090712">
+<node CREATED="1246407978066" ID="Freemind_Link_924544648" MODIFIED="1246407979659" TEXT="&#x30bf;&#x30b9;&#x30af;">
+<node CREATED="1246408259093" ID="Freemind_Link_353875526" MODIFIED="1246408263623" TEXT="&#x30c6;&#x30b9;&#x30c8;&#x4f5c;&#x6210;"/>
+<node CREATED="1246407925684" ID="Freemind_Link_352648985" MODIFIED="1246413057495" TEXT="UI&#x30af;&#x30e9;&#x30b9;&#xff08;Google Android&#xff09;&#x306e;&#x958b;&#x767a;">
+<node CREATED="1246408627433" ID="Freemind_Link_386106585" MODIFIED="1246408651337" TEXT="&#x958b;&#x767a;&#x8005;&#x3092;&#x5897;&#x54e1;">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1246408082862" ID="Freemind_Link_58217775" MODIFIED="1246408115039" TEXT="AI&#x30af;&#x30e9;&#x30b9;&#x306e;&#x958b;&#x767a;">
+<node CREATED="1246408627433" ID="Freemind_Link_1138686863" MODIFIED="1246408651337" TEXT="&#x958b;&#x767a;&#x8005;&#x3092;&#x5897;&#x54e1;">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+<node CREATED="1246407946159" ID="Freemind_Link_566665103" MODIFIED="1246407949313" TEXT="&#x30ea;&#x30ea;&#x30fc;&#x30b9;">
+<node CREATED="1246407953437" ID="Freemind_Link_402626327" MODIFIED="1246408604839" TEXT="Google Android&#x3067;&#x52d5;&#x304f;&#x9ebb;&#x96c0;&#x30a2;&#x30d7;&#x30ea;&#xff08;&#x30a2;&#x30eb;&#x30d5;&#x30a1;&#x7248;&#xff09;">
+<icon BUILTIN="licq"/>
+</node>
+</node>
+</node>
+<node CREATED="1246408145265" ID="Freemind_Link_686460058" MODIFIED="1246408153669" TEXT="&#xff5e;20090719">
+<node CREATED="1246408163213" ID="Freemind_Link_939419195" MODIFIED="1246408165603" TEXT="&#x30bf;&#x30b9;&#x30af;">
+<node CREATED="1246407925684" ID="Freemind_Link_1285891187" MODIFIED="1246413060884" TEXT="UI&#x30af;&#x30e9;&#x30b9;&#xff08;Google Android&#xff09;&#x306e;&#x5f37;&#x5316;">
+<node CREATED="1246408627433" ID="Freemind_Link_1850293948" MODIFIED="1246408697991" TEXT="&#x30c6;&#x30b9;&#x30c8;&#x6280;&#x8853;&#x8005;&#x3092;&#x5c0e;&#x5165;">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1246408082862" ID="Freemind_Link_1554730527" MODIFIED="1246408115039" TEXT="AI&#x30af;&#x30e9;&#x30b9;&#x306e;&#x958b;&#x767a;"/>
+<node CREATED="1246408427548" ID="Freemind_Link_754679327" MODIFIED="1246408434390" TEXT="&#x5404;&#x6a5f;&#x80fd;&#x306e;&#x30ea;&#x30d5;&#x30a1;&#x30af;&#x30bf;&#x30ea;&#x30f3;&#x30b0;"/>
+</node>
+<node CREATED="1246408165884" ID="Freemind_Link_216076189" MODIFIED="1246408168181" TEXT="&#x30ea;&#x30ea;&#x30fc;&#x30b9;">
+<node CREATED="1246407953437" ID="Freemind_Link_1941720032" MODIFIED="1246408608725" TEXT="Google Android&#x3067;&#x52d5;&#x304f;&#x9ebb;&#x96c0;&#x30a2;&#x30d7;&#x30ea;&#xff08;&#x30d9;&#x30fc;&#x30bf;&#x7248;&#xff09;">
+<icon BUILTIN="licq"/>
+</node>
+</node>
+</node>
+<node CREATED="1246408471476" ID="Freemind_Link_1081497464" MODIFIED="1246408486957" TEXT="&#xff5e;20090726">
+<node CREATED="1246408546499" ID="Freemind_Link_942203530" MODIFIED="1246408548792" TEXT="&#x30bf;&#x30b9;&#x30af;">
+<node CREATED="1246408551414" ID="Freemind_Link_1897409836" MODIFIED="1246408553146" TEXT="&#x30c6;&#x30b9;&#x30c8;">
+<node CREATED="1246408627433" ID="Freemind_Link_1031036631" MODIFIED="1246408687724" TEXT="&#x30c6;&#x30b9;&#x30c8;&#x6280;&#x8853;&#x8005;&#x3092;&#x5897;&#x54e1;">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1246408553817" ID="Freemind_Link_636850107" MODIFIED="1246408557374" TEXT="&#x30d0;&#x30b0;&#x6539;&#x4fee;"/>
+</node>
+<node CREATED="1246408489300" ID="Freemind_Link_1526122007" MODIFIED="1246408491581" TEXT="&#x30ea;&#x30ea;&#x30fc;&#x30b9;">
+<node CREATED="1246407953437" ID="Freemind_Link_1414928993" MODIFIED="1246408611642" TEXT="Google Android&#x3067;&#x52d5;&#x304f;&#x9ebb;&#x96c0;&#x30a2;&#x30d7;&#x30ea;&#xff08;&#x6b63;&#x5f0f;&#x7248;&#xff09;">
+<icon BUILTIN="bookmark"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>

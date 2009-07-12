568d95757b6115ffb4eef3a727f2b2adb7abef6a
mekimeki1980812 <mekimeki1980812@867d8d17-13de-45a2-9b0c-8d4e0d86bef1>
2009-07-12 16:07:23 +0000

    [T.Nakmura 2009/07/13 1:05] 設定画面に"くいたん"「あり」「なし」を追加。

--- programs/Android/Andjong/Andjong/src/org/andjong/Settings.java
+++ programs/Android/Andjong/Andjong/src/org/andjong/Settings.java
@@ -1,12 +1,24 @@
 package org.andjong;
 
-import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import org.andjong.R.id;
 
-public class Settings extends PreferenceActivity {
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+
+public class Settings extends Activity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.settings);
+
+		setContentView(R.layout.settings);
+
+		final Spinner spin1 = (Spinner) findViewById(id.tanyao_setting);
+		String[] arr = {"‚ ‚è","‚È‚µ"};
+		
+		ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list, arr);
+		spin1.setAdapter(adapter);
+		spin1.setSelection(0);
 	}
 }

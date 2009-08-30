fca8a60fe8fbc6f8ee14e7256c5de9a8b3d6e6b9
urushibaray <urushibaray@867d8d17-13de-45a2-9b0c-8d4e0d86bef1>
2009-08-30 14:50:53 +0000

    表示部分を作成しました。

--- programs/practice/Azana/src/com/bonpu/azana/Azana.java
+++ programs/practice/Azana/src/com/bonpu/azana/Azana.java
@@ -4,8 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Azana extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+	/** Called when the activity is first created. */
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
